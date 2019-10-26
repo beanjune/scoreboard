32aa59ebe212a41197ca5e2ac0f1f6d7dad94f10
Na sangwoon <swna8934@gmaiil.com>
2019-10-26 16:44:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import {Header} from './components/header';
-import {Counter} from './components/counter';
-
-
-//함수를 선언한 결과물이 와야하기때문에 ()를 하면 안된다.
-//this를 독립적으로 존재해 까먹어 this 바인드로 묶어줘야한다. 멕시컬 THIS방법도 있다.
-//state 를 변경하는 방법은 setSate밖에 없다.
-
-const Player = (props) => (
-    <div className="player">
-            <span className="player-name">
-                <button className="remove-player" onClick={() => props.removePlayer(props.id)}>x</button>
-                {props.name}
-            </span>
-        <Counter score={props.score}/>
-    </div>
-)
-//함수선언문으로 넣어야하기때문에 익명함수로 한번 감싸서 한다.
-
+import {Player} from "./components/player";
 
 class App extends React.Component {
 
