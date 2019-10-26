c6b4b8628ba623d98fec5efdf4697d2825ab01fe
Na sangwoon <swna8934@gmaiil.com>
2019-10-26 16:22:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import React from 'react';
 import './App.css'
 import {Header} from './components/header';
+import {Counter} from './components/counter';
 
-
-
-//fucntion 컴포넌트를 class 컴포넌트로 전환
-class Counter extends React.Component {
-
-  state = {
-    score: 0,
-    name:'aa'
-  }
-
-  incrementScore() {
-    console.log('increment',this);
-    //this.state.score += 1; 안된다.
-    // this.setState({score: this.state.score +1}); 동기식
-    //this.setState(prevState => {
-    //    return {score: prevState.score +1}
-    //});
-
-    this.setState(prevState => ({score: prevState.score +1}));
-    //비동기식 return 이한줄이라 지울수 있는데 제이슨인지함수인지 몰라서 가로를 감싸주어 생략가능
-  }
-
-  render() {
-    return(
-        <div className="counter">
-          <button className="counter-action decrement">-</button>
-          <span className="counter-score">{this.state.score}</span>
-          <button className="counter-action increment" onClick={this.incrementScore.bind(this)}>+</button>
-        </div>
-    )
-  }
-
-}
 
 //함수를 선언한 결과물이 와야하기때문에 ()를 하면 안된다.
 //this를 독립적으로 존재해 까먹어 this 바인드로 묶어줘야한다. 멕시컬 THIS방법도 있다.
@@ -44,12 +12,13 @@ const Player = (props) => (
     <div className="player">
             <span className="player-name">
                 <button className="remove-player" onClick={() => props.removePlayer(props.id)}>x</button>
-              {props.name}
+                {props.name}
             </span>
-      <Counter score={props.score}/>
+        <Counter score={props.score}/>
     </div>
 )
 //함수선언문으로 넣어야하기때문에 익명함수로 한번 감싸서 한다.
+
 
 class App extends React.Component {
 
